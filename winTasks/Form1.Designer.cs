aea2d2690d9b6134501c05590fd5a88da1db0f46
FC <fercor1988@gmail.com>
2018-02-27 06:18:07 -0500

    first commit App

---
+++ winTasks/Form1.Designer.cs
@@ -0,0 +1,146 @@
+﻿namespace winTasks
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador requerida.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén utilizando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido del método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.button1 = new System.Windows.Forms.Button();
+            this.progressBar2 = new System.Windows.Forms.ProgressBar();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.progressBar3 = new System.Windows.Forms.ProgressBar();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.progressBar4 = new System.Windows.Forms.ProgressBar();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 74);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(107, 184);
+            this.textBox1.TabIndex = 0;
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(12, 57);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(107, 11);
+            this.progressBar1.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // progressBar2
+            // 
+            this.progressBar2.Location = new System.Drawing.Point(125, 57);
+            this.progressBar2.Name = "progressBar2";
+            this.progressBar2.Size = new System.Drawing.Size(107, 11);
+            this.progressBar2.TabIndex = 4;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(125, 74);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(107, 184);
+            this.textBox2.TabIndex = 3;
+            // 
+            // progressBar3
+            // 
+            this.progressBar3.Location = new System.Drawing.Point(238, 57);
+            this.progressBar3.Name = "progressBar3";
+            this.progressBar3.Size = new System.Drawing.Size(107, 11);
+            this.progressBar3.TabIndex = 6;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(238, 74);
+            this.textBox3.Multiline = true;
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(107, 184);
+            this.textBox3.TabIndex = 5;
+            // 
+            // progressBar4
+            // 
+            this.progressBar4.Location = new System.Drawing.Point(351, 57);
+            this.progressBar4.Name = "progressBar4";
+            this.progressBar4.Size = new System.Drawing.Size(107, 11);
+            this.progressBar4.TabIndex = 8;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(351, 74);
+            this.textBox4.Multiline = true;
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(107, 184);
+            this.textBox4.TabIndex = 7;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(469, 270);
+            this.Controls.Add(this.progressBar4);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.progressBar3);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.progressBar2);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ProgressBar progressBar2;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.ProgressBar progressBar3;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.ProgressBar progressBar4;
+        private System.Windows.Forms.TextBox textBox4;
+    }
+}
+
---
+++ winTasks/Form1.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace winTasks
+{
+    public partial class Form1 : Form
+    {
+        private TaskScheduler _scheduler;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            _scheduler = TaskScheduler.FromCurrentSynchronizationContext();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            new Task(() =>
+            {
+                Task[] tareas = {
+                Task.Factory.StartNew(() => IncreaseValue(textBox1, progressBar1, 150, 1), CancellationToken.None, TaskCreationOptions.LongRunning, _scheduler),
+                Task.Factory.StartNew(() => IncreaseValue(textBox2, progressBar2, 180, 1), CancellationToken.None, TaskCreationOptions.LongRunning, _scheduler),
+                Task.Factory.StartNew(() => IncreaseValue(textBox3, progressBar3, 50, 1), CancellationToken.None, TaskCreationOptions.LongRunning, _scheduler),
+                Task.Factory.StartNew(() => IncreaseValue(textBox4, progressBar4, 200, 1), CancellationToken.None, TaskCreationOptions.LongRunning, _scheduler)
+                };
+                Task.WaitAll(tareas);
+
+            }).Start();
+
+            MessageBox.Show("Se han terminado todas las tareas");
+        }
+
+        private void IncreaseValue(TextBox t, ProgressBar p, int r, int c)
+        {
+            p.Maximum = r;
+            for (int i = 0; i < r; i++)
+            {
+                int result = i * c;
+
+                Thread.Sleep(10);
+
+                UpdateView(t, p, result);
+            }
+        }
+
+        private void UpdateView(TextBox t, ProgressBar p, int result)
+        {
+            p.Value = result;
+            t.Text += "Count: " + result + Environment.NewLine;
+            t.SelectionStart = t.Text.Length;
+            t.ScrollToCaret();
+            Application.DoEvents();
+        }
+
+    }
+}
